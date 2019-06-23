df38ad9bdccc1bf4339a2bc2aec813e668f6e36b
lg0812 <zhangrenwu@ikcrm.com>
2019-06-23 21:42:10 +0800

    'init'

---
+++ mscloud-provider-dept-8001/src/main/java/com/yltfy/springcloud/service/DeptService.java
@@ -0,0 +1,13 @@
+package com.yltfy.springcloud.service;
+
+import com.yltfy.springcloud.entities.Dept;
+
+import java.util.List;
+
+public interface DeptService {
+    public boolean add(Dept dept);
+
+    public Dept get(Long id);
+
+    public List<Dept> list();
+}
