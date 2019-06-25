516164a2ba08b41ce99f930fdffb9171c40ba576
zrw <zhangrenwu@ikcrm.com>
2019-06-25 09:40:28 +0800

    delimit $ => @

--- mscloud-provider-dept-8001/src/main/java/com/yltfy/springcloud/controller/DeptController.java
+++ mscloud-provider-dept-8001/src/main/java/com/yltfy/springcloud/controller/DeptController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.yltfy.springcloud.entities.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,9 @@ import com.yltfy.springcloud.service.DeptService;
 public class DeptController {
     @Autowired
     private DeptService service;
+
+    @Autowired
+    private DiscoveryClient client;
 
     @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
     public boolean add(@RequestBody Dept dept) {
@@ -29,5 +34,17 @@ public class DeptController {
     @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
     public List<Dept> list() {
         return service.list();
+    }
+
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery() {
+        List<String> list = client.getServices();
+        System.out.println("**********" + list);
+        List<ServiceInstance> srvList = client.getInstances("MSCLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t" + element.getUri());
+        }
+        return this.client;
+
     }
 }
